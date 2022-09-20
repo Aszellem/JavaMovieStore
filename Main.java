4c496cc4beaa5ae8248d53c4dd5ae3a3dffd0375
aszellem <aszellemek@gmail.com>
2022-09-20 15:15:23 +0200

    done

--- Main.java
+++ Main.java
@@ -1,35 +1,91 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import models.*;
 
 public class Main {
+    static Store store = new Store();
 
     public static void main(String[] args) {
         System.out.println("\n********************JAVA VIDEO STORE********************\n");
-        Movie movie = new Movie("Kill Bill", "Blue-Ray", 9.0);
-        System.out.println(movie);
+        try {
+            loadMovies("movies.txt");
+            System.out.print("MOVIES LOADED\n\n");
+            System.out.println(store);
+            manageMovies();
 
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
      * Name: manageMovies
      * Inside the function:
-     *   • 1. Starts a new instance of Scanner;
-     *   • 2. In an infinite loop, the user can choose to a) purchase b) rent c) return d) exit.
-     *   •        case a: ask for the name and sell.
-     *   •        case b: ask for the name and rent.
-     *   •        case c: ask for the name and return.
-     *   • 3. call close() from the Scanner object.
+     * • 1. Starts a new instance of Scanner;
+     * • 2. In an infinite loop, the user can choose to a) purchase b) rent c)
+     * return d) exit.
+     * • case a: ask for the name and sell.
+     * • case b: ask for the name and rent.
+     * • case c: ask for the name and return.
+     * • 3. call close() from the Scanner object.
      */
 
+    public static void manageMovies() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nWould you like to \n\ta) purchase\n\tb) rent \n\tc) return");
+            String choose = scan.nextLine();
+            if (!(choose.equals("a") || choose.equals("b") || choose.equals("c"))) {
+                scan.close();
+                break;
+            }
+
+            System.out.println("Enter the name of the movie: ");
+            String movieName = scan.nextLine();
+            if(store.getMovie(movieName) == null){
+                System.out.println("\n\nThe input you provided is not valid. Please try again\n");
+                continue;
+            }
+
+            switch(choose){
+                case "a": 
+                if(!(store.getMovie(movieName).isAvailable())){
+                    System.out.println("\n\n\n\nThe movie is not available for purchase. Please try again\n");
+                    continue;
+                }
+                store.action(movieName, "sell"); break;
+                case "b": store.action(movieName, "rent"); break;
+                case "c": store.action(movieName, "rental"); break;
+            }
+            System.out.println("\n\nUPDATED MOVIES\n\n" + store);
+        }
+    }
 
     /**
      * Name: loadMovies
+     * 
      * @param fileName (String)
      * @throws FileNotFoundException
      *
-     * Inside the function:
-     *   • 1. loads movies from <fileName>.txt.
-     *   • 2. adds all movies to the store object's movie field.
-     *        Hint: You will need to 'split' a String into three Strings.
+     *                               Inside the function:
+     *                               • 1. loads movies from <fileName>.txt.
+     *                               • 2. adds all movies to the store object's
+     *                               movie field.
+     *                               Hint: You will need to 'split' a String into
+     *                               three Strings.
      */
+
+    public static void loadMovies(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scan = new Scanner(file);
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] words = line.split("--");
+            store.addMovie(new Movie(words[0], words[1], Double.parseDouble(words[2])));
+        }
+        scan.close();
+    }
 
 }
--- models/Store.java
+++ models/Store.java
@@ -1,5 +1,75 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Store {
+    ArrayList<Movie> movies;
+
+    public Store() {
+        this.movies = new ArrayList<Movie>();
+    }
+
+    public Movie getMovie(int index) {
+        return new Movie(movies.get(index));
+    }
+
+    public void setMovie(int index, Movie movie) {
+        this.movies.set(index, new Movie(movie));
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(new Movie(movie));
+    }
+
+    public void action(String movieName, String action) {
+        if(movies.isEmpty()){
+            throw new IllegalStateException("The store is not in a valid state to call the action method.");
+        }
+        if(!(action.equals("sell") || action.equals("rent") || action.equals("return"))){
+            throw new IllegalArgumentException("");
+        }
+        if(movieName == null || movieName.isBlank()){
+            throw new IllegalArgumentException("The name cannot be blank");
+        }
+        for (int i = 0; i < movies.size(); i++) {
+            if (movies.get(i).getName().equals(movieName)) {
+                switch (action) {
+                    case "sell":
+                        movies.remove(i);
+                        if (!(movies.get(i).isAvailable())) {
+                            throw new IllegalStateException("Cannot sell movie that was rented out");
+                        }
+                        break;
+                    case "rent":
+                        movies.get(i).setAvailable(false);
+                        break;
+                    case "return":
+                        movies.get(i).setAvailable(true);
+                        break;
+                }
+            }
+        }
+    }
+
+    public Movie getMovie(String name){
+        for (int i = 0; i < movies.size(); i++) {
+            if(movies.get(i).getName().equals(name)){
+                return new Movie(movies.get(i));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.movies.size(); i++) {
+            temp += this.movies.get(i).toString();
+            temp += "\n\n";
+        }
+        return temp;
+    }
+
     
+
 }
